ace68ea0a5f1f76536226f2f7321a0bb1eaad442
madzhuga <maximmadzhuga@gmail.com>
2014-05-02 10:43:23 +0400

    database config updates

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 /*global module */
 module.exports = {
-    'url' : 'mongodb://127.0.0.1:27017/node_tumba' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+//    'url' : 'mongodb://127.0.0.1:27017/node_tumba' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://127.0.0.1:27017/tumba'
 };
